69dca6d05257327357a898cd586f712a2cce3300
maskikuy <kproject305@gmail.com>
2024-04-05 15:52:14 +0700

    masukan while

--- AlgorithmaSearch_0060/AlgorithmaSearch_0060.cpp
+++ AlgorithmaSearch_0060/AlgorithmaSearch_0060.cpp
@@ -38,3 +38,9 @@ void LinearSearch()
     {
         cout << "\nEnter the element you want to search : ";
         cin >> item;
+
+        ctr = 0;
+        i = 0;
+
+        while (i < n)
+        {
