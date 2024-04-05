3b9399d17dec0d0e0326637dba29e301b5c5ebca
maskikuy <kproject305@gmail.com>
2024-04-05 15:49:34 +0700

    array harus memiliki minimal 1 atau maksimal 20 elemen

--- AlgorithmaSearch_0060/AlgorithmaSearch_0060.cpp
+++ AlgorithmaSearch_0060/AlgorithmaSearch_0060.cpp
@@ -11,3 +11,9 @@ void input()
     {
         cout << "Enter the number of elements in the Array : ";
         cin >> n;
+
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
